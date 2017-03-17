54b9183e4fe99e95396a8fd53d54603d8d454596
Daniel Colangelo <Danalfons@yahoo.com>
2017-03-17 03:33:43 -0400

    a medication picker app

---
+++ miracle-pills/miracle-pills/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  miracle-pills
+//
+//  Created by Danny Colangelo on 3/17/17.
+//  Copyright © 2017 Danny Colangelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
